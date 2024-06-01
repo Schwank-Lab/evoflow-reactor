c25825968c6d3bf1cf89066d2a73dd6ce3dd7a81
Sasha M <sasha.melkonyan@gmail.com>
2024-06-01 10:08:35 +0200

    add a command line option to update reactor config

--- client/commands.py
+++ client/commands.py
@@ -10,7 +10,7 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Connection failed with code {rc}")
 
-def load_experiment_config(config_path):
+def load_config(config_path):
     with open(config_path, 'r') as file:
         return json.load(file)
 
@@ -21,8 +21,10 @@ def create_payload(args):
     }
     if args.experiment_id:
         payload["experiment_id"] = args.experiment_id
-    if args.config:
-        payload["experiment_config"] = load_experiment_config(args.config)
+    if args.config and args.command in ['new_experiment', 'update_experiment_config']:
+        payload["experiment_config"] = load_config(args.config)
+    if args.config and args.command == 'update_reactor_config':
+        payload['reactor_config'] = load_config(args.config)
     if args.stepper_vol:
         payload["stepper_vol"] = args.stepper_vol
     return payload
@@ -31,7 +33,8 @@ def create_payload(args):
 parser = argparse.ArgumentParser(description='Send commands to the experiments via MQTT.')
 parser.add_argument('reactor_id', type=str, help='ID of the reactor')
 parser.add_argument('command', type=str, choices=['start', 'stop', 'pause', 'new_experiment', 'update_experiment_config', 
-                                                  'stepper_forward', 'stepper_reverse', 'stepper_stop'],
+                                                  'stepper_forward', 'stepper_reverse', 'stepper_stop',
+                                                  'update_reactor_config'],
                     help='Command to send to the reactor')
 parser.add_argument('--experiment_id', type=str, help='ID of the experiment (required for new_experiment and update_experiment_config)')
 parser.add_argument('--config', type=str, help='Path to the experiment configuration file (required for new_experiment and update_experiment_config)')
@@ -44,10 +47,16 @@ args = parser.parse_args()
 if args.command in ['new_experiment', 'update_experiment_config']:
     if not args.experiment_id or not args.config:
         parser.error("experiment_id and config are required for new_experiment and update_experiment_config commands")
+    if not args.config: 
+        parser.error("config is required for new_experiment and update_experiment_config commands")
 
 if args.command in ['stepper_forward', 'stepper_reverse']:
     if not args.stepper_vol:
         parser.error("stepper_vol is required for stepper_forward and stepper_reverse commands")
+
+if args.command == 'update_reactor_config':
+    if not args.config:
+        parser.error("config is required for update_reactor_config command")
 
 # Create the MQTT client
 client = mqtt.Client()
