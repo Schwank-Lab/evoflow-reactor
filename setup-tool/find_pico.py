1013fd1d8ad9b64c27dd2cc9ccb35169f8134d33
graziano <gm@me.com>
2024-07-04 11:08:08 +0200

    now tell the user how to export the variable

--- setup-tool/find_pico.py
+++ setup-tool/find_pico.py
@@ -1,6 +1,9 @@
+import platform
+import subprocess
 import serial.tools.list_ports
-import os 
+import os
 import tempfile
+
 
 def find_pico_port():
     ports = serial.tools.list_ports.comports()
@@ -9,20 +12,22 @@ def find_pico_port():
             return port.device
     return None
 
+
 def ensure_tmp_dir():
     tmp_dir = tempfile.gettempdir()
     if not os.path.exists(tmp_dir):
         os.makedirs(tmp_dir)
     return tmp_dir
 
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
     port = find_pico_port()
-    if port is None: 
-        print('Could not find the pico attached.')
-    else: 
+    if port is None:
+        print("Could not find the pico attached.")
+    else:
         tmp_dir = ensure_tmp_dir()
-        file_path = os.path.join(tmp_dir, 'pico_port.txt')
-        print(f'Pico is found at port:\n{port}')
-        with open(file_path, 'w') as f: 
+        file_path = os.path.join(tmp_dir, "pico_port.txt")
+        print(f"Pico is found at port: {port}")
+        with open(file_path, "w") as f:
             f.write(port)
-        print(f'pico port info saved on {file_path}')
+        print(f"Copy/Paste (Run) the following command: 'export PICO_PORT={port}'")
